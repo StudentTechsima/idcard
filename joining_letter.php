95820dd496e843e86077a92f00cf45300ae2dee0
Rani <ranimaurya591@gmail.com>
2025-01-30 19:35:28 +0530

    joining letter

---
+++ joining_letter.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Code of Conduct Bureau Letter</title>
+    <style>
+        
+        .container {
+            max-width: 800px;
+            background: white;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border:2px solid #741b16;
+        }
+       h3,h4 {
+            color: #741b16;
+            text-align: center;
+        }
+        .logo {
+            display: block;
+            margin: 0 auto;
+            width: 100px;
+        }
+        .letter-content {
+            line-height: 1.6;
+            padding:10px 30px;
+        }
+        .from-content-header{
+            display:flex;
+            align-items:start   ;
+            justify-content:space-between;
+        }
+        .sign{
+            text-align:center;
+        }
+        .footer {
+            height: 60px;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            background: #741b16;
+            align-items: center;
+            margin-top: 20px;
+            font-size: 14px;
+            text-align: center;
+            color: #ffffff;
+            padding:0px;
+        }
+        .footer a{
+            color:#fff;
+            text-decoration:none;
+            cursor:pointer;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <img src="images/logo.png" alt="CCB Logo" class="logo">
+        <div class="letter-content">
+            <div class="from-content-header">
+            <div class="from-content">
+            <p>
+                <strong>To:</strong> Mr. Ibrahim Mustapha Magu<br>
+                Former Acting Chairman,<br>
+                Economic & Financial Crimes Commission (EFCC),<br>
+                Plot 177, Karu Site,<br>
+                Karu, Abuja.
+            </p>
+            </div>
+            <div class=emp_id>
+                <p><strong>Ref No:</strong> CCB/HQ/18M/007/2093</p>
+                <p><strong>Date:</strong> 2nd November, 2020</p>
+             </div>
+             </div>
+            
+            <h3>INVESTIGATION ACTIVITIES: LETTER OF INVITATION</h3>
+            <h4>BREACH OF CODE OF CONDUCT FOR PUBLIC OFFICERS</h4>
+            <p>The Bureau is investigating a case of alleged breach of the Code of Conduct for Public Officers against Mr. Ibrahim Mustapha Magu, former Acting Chairman of the Economic & Financial Crimes Commission (EFCC).</p>
+            <p>You are invited for an interview scheduled as follows:</p>
+            <ul>
+                <li><strong>Date:</strong> Tuesday 17th November, 2020</li>
+                <li><strong>Time:</strong> 11:00am Prompt</li>
+                <li><strong>Venue:</strong> CCB Interview Room, 5th Floor, Annex 3, Federal Secretariat Complex, Shehu Shagari Way, Maitama, Abuja.</li>
+            </ul>
+            <p>You are expected to come along with the following certified documents:</p>
+            <ul>
+                <li>Acknowledgment slips of all your Asset Declarations since joining public service.</li>
+                <li>Copies of appointment letter, acceptance, records of service, and pay slips from January to May 2020.</li>
+                <li>Title documents of your landed properties (both developed and undeveloped).</li>
+                <li>All bank account statements from January 2015 to date.</li>
+            </ul>
+            <p>Treat as urgent, please.</p>
+            <p class="sign"><strong>Signed,</strong><br>
+                <strong>Gyimi S. Y.</strong><br>
+                Director of Intelligence, Investigation & Monitoring<br>
+                For: Chairman, CCB
+            </p>
+        </div>
+        <div class="footer">
+            <p>For more details, visit <a href="https://jagritinews.com/" target="_blank">© 2023 Reserved Jagriti News </a></p>
+        </div>
+    </div>
+</body>
+</html>
---
+++ joining_letter_form.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ABPA Member Registration</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body class="bg-white">
+    <div class="container py-5">
+        <form class="row" enctype="multipart/form-data">
+            <div class="col-lg-3"></div>
+            <div class="col-lg-6 form p-4 bg-light rounded-3">
+                    <div class="mb-3">
+                        <label for="address" class="form-label">Address*</label>
+                        <textarea id="address" name="address" class="form-control" rows="3"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="address" class="form-label">Address*</label>
+                        <textarea id="address" name="address" class="form-control" rows="3"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="address" class="form-label">Address*</label>
+                        <textarea id="address" name="address" class="form-control" rows="3"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="address" class="form-label">Address*</label>
+                        <textarea id="address" name="address" class="form-control" rows="3"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="address" class="form-label">Address*</label>
+                        <textarea id="address" name="address" class="form-control" rows="3"></textarea>
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-success">Joining Letter</button>
+                    </div>                   
+                <div class="col-lg-3"></div>
+        <form>
+    </div>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery.js"></script>
+</body>
+
+</html>
